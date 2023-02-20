5880a799a33f28fce950ff5c71123b402d16e6b4
Hamza <hamzaensi@gmail.com>
2023-02-20 14:30:09 +0100

    mise à jours

--- tasklist/app.js
+++ tasklist/app.js
@@ -30,40 +30,7 @@ function loadEventListeners() {
 function addTask(e)
 {
   
-  if(taskInput.value == '')
-  {
-    alert('Add task');
 
-  }
-  else
-  {
-  
-  // Create li element
-  const li = document.createElement('li');
-   // Add class
-   li.className = 'collection-item';
-   // Create text node and append to li
-  li.appendChild(document.createTextNode(taskInput.value));
-   
-
-  
-  // Create new link element
-   const link = document.createElement('a');
-   // Add class
-   link.className = 'delete-item secondary-content';
-   // Add icon html
-   link.innerHTML = '<i class="fa fa-remove"></i>';
-   // Append the link to li
-   li.appendChild(link);
-
-   // Append li to ul
-   taskList.appendChild(li);
-   // Clear input
-  taskInput.value = '';
-
-  }
-   e.preventDefault();  
-   
 
 }
 
@@ -73,16 +40,12 @@ function getTasks()
 }
 function removeTask(e)
 {
-  if(e.target.parentElement.classList.contains('delete-item')) {
-    if(confirm('Are You Sure?')) {
-      e.target.parentElement.parentElement.remove();
-    }
-  }
+  
   
 }
 function clearTasks()
 {
-  alert('Clear tasks');
+  
 }
 function filterTasks()
 {
