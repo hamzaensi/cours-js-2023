5a5964d9f51a1ec263010c580395e3a3d02d97bd
hamza <hamzaensi@gmail.com>
2023-02-20 09:26:53 +0100

    PRIMITIVE

--- app.js
+++ app.js
@@ -90,3 +90,32 @@ console.timeEnd('Hello');
 // numbers.push(6);
 
 // console.log(numbers);
+
+
+// Basic 3 
+  // PRIMITIVE
+
+// String
+const name = 'John Doe';
+// Number
+const age = 45;
+// Boolean
+const hasKids = true;
+// Null
+const car = null;
+// Undefined
+let test;
+// Symbol
+const sym = Symbol();
+
+// REFERENCE TYPES - Objects
+// Array
+const hobbies = ['movies', 'music'];
+// Object literal
+const address = {
+  city: 'Boston',
+  state: 'MA'
+}
+const today = new Date();
+console.log(today);
+console.log(typeof today);
--- cours.js
+++ cours.js
@@ -8,20 +8,7 @@
 
 
 /*
-const person = {
-    name: 'John',
-    age: 30
-  }
-  
-  person.name = 'Sara';
-  person.age = 32;
-  
-  // console.log(person);
-  
-  const numbers = [1,2,3,4,5];
-  numbers.push(6);
-  
-  console.log(numbers);
+
 
 
   // Basic 3 
