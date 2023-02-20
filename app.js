4107056e12a111cbae2bdb9a4c3bb08568dd8fff
hamza <hamzaensi@gmail.com>
2023-02-20 09:40:35 +0100

    conversion

--- app.js
+++ app.js
@@ -95,27 +95,85 @@ console.timeEnd('Hello');
 // Basic 3 
   // PRIMITIVE
 
-// String
-const name = 'John Doe';
-// Number
-const age = 45;
-// Boolean
-const hasKids = true;
-// Null
-const car = null;
-// Undefined
-let test;
-// Symbol
-const sym = Symbol();
+// // String
+// const name = 'John Doe';
+// // Number
+// const age = 45;
+// // Boolean
+// const hasKids = true;
+// // Null
+// const car = null;
+// // Undefined
+// let test;
+// // Symbol
+// const sym = Symbol();
 
-// REFERENCE TYPES - Objects
-// Array
-const hobbies = ['movies', 'music'];
-// Object literal
-const address = {
-  city: 'Boston',
-  state: 'MA'
-}
-const today = new Date();
-console.log(today);
-console.log(typeof today);
+// // REFERENCE TYPES - Objects
+// // Array
+// const hobbies = ['movies', 'music'];
+// // Object literal
+// const address = {
+//   city: 'Boston',
+//   state: 'MA'
+// }
+// const today = new Date();
+// console.log(today);
+// console.log(typeof today);
+
+
+// Basic 4 
+
+let val;
+
+// Number to string
+val = String(555);
+
+
+
+val = String(4+4);
+
+
+// Bool to string
+val = String(true);
+
+// Date to string
+val = String(new Date());
+
+// Array to string
+val = String([1,2,3,4]);
+
+
+// // toString()
+// val = (5).toString();
+// val = (true).toString();
+
+// // String to number
+val = Number('5');
+
+val = Number(false);
+
+// val = Number(false);
+val = Number(null);
+
+val = Number('hello');
+
+val = Number([1,2,3]);
+
+
+val = parseFloat('100.30');
+
+// val = parseFloat('100.30');
+console.log(val);
+// // Output
+// // console.log(val);
+// // console.log(typeof val);
+// // console.log(val.length);
+// // console.log(val.toFixed(2));
+
+const val1 = String(5);
+const val2 = 6;
+
+const sum = Number(val1) + val2;
+
+console.log(sum);
+// console.log(typeof sum);
