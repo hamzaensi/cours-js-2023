110fdf0f5343945c4d6084377e6da3c55f76fa5b
hamza <hamzaensi@gmail.com>
2023-02-27 10:14:24 +0100

    mise à jours

--- app.js
+++ app.js
@@ -122,7 +122,7 @@ console.timeEnd('Hello');
 
 
 // Basic 4 
-
+/*
 let val;
 
 // Number to string
@@ -177,3 +177,153 @@ const sum = Number(val1) + val2;
 
 console.log(sum);
 // console.log(typeof sum);
+
+*/
+/*
+// Basic 5 
+
+const num1 = 100;
+const num2 = 50;
+let val;
+
+// Simple math with numbers
+val = num1 + num2;
+
+
+ 
+val = num1 * num2;
+
+val = num1 - num2;
+val = num1 / num2;
+
+val = num1 % num2;
+
+
+
+// Math Object
+val = Math.PI;
+
+val = Math.E;
+
+val = Math.round(2.5);
+
+val = Math.ceil(2.4);
+
+val = Math.floor(2.8);
+
+val = Math.sqrt(64);
+
+val = Math.abs(-3);
+//val = Math.sqrt(Math.pow(8, 2));
+
+val = Math.pow(7, 2);
+
+val = Math.min(2,33,4,1,55,6,3,-2);
+
+val = Math.max(2,33,4,1,55,6,3,-2);
+
+val = Math.random();
+
+val = Math.floor(Math.random() * 50 + 7);
+
+console.log(val);
+
+*/
+
+// Basic 6 
+
+const firstName = 'William';
+const lastName = 'Johnson';
+const age = 36;
+const str = 'Brad Hello there my name is Brad and Brad';
+const tags = 'web design,web development,programming';
+
+let val;
+
+val = firstName + lastName;
+
+
+
+
+
+// Concatenation
+val = firstName + ' ' + lastName;
+
+
+// Append
+val = 'Brad ';
+val += 'Traversy';
+
+val = 'Hello, my name is ' + firstName + ' and I am ' + age;
+
+
+
+// Escaping
+val =  "That's awesome, I can't wait";
+
+// Length
+val = firstName.length;
+
+
+
+// concat()
+val = firstName.concat(' ', lastName);
+
+// Change case
+val = firstName.toUpperCase();
+
+val = firstName.toLowerCase();
+
+val = firstName[2];
+
+// indexOf()
+val = firstName.indexOf('l');
+
+val = firstName.lastIndexOf('l');
+
+// charAt()
+val = firstName.charAt('2');
+
+// Get last char
+val = firstName.charAt(firstName.length - 1);
+//William
+// substring()
+val = firstName.substring(0, 4);
+
+// slice()
+val = firstName.slice(0,4);
+
+val = firstName.slice(-5,-2);//William
+
+// split()
+val = str.split(' ');
+
+val = tags.split(',');
+
+// replace()
+val = str.replace('Brad', 'Jack');
+
+// includes()
+val = str.includes('Brad');
+
+
+ch=""
+v=str.split(" ");
+for(let i=0;i<v.length;i++)
+{
+  if (v[i]=='Brad')
+  
+    v[i]='Jack';
+  
+}
+ch=String(v);
+ch=ch.replaceAll(","," ");
+console.log(ch);
+
+
+
+
+
+
+
+ 
