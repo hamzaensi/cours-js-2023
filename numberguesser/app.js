29c6d2b3b2a97c1130acc40823d004ba3ec48061
Hamza Zagrouba <hamzaensi@gmail.com>
2023-03-13 12:18:50 +0100

    Update app.js

--- numberguesser/app.js
+++ numberguesser/app.js
@@ -14,7 +14,7 @@ let min = 1,
     guessesLeft = 3;
 
 // UI Elements
-const game = document.querySelector('#game'),
+      const game = document.querySelector('#game'),
       minNum = document.querySelector('.min-num'),
       maxNum = document.querySelector('.max-num'),
       guessBtn = document.querySelector('#guess-btn'),
@@ -42,48 +42,58 @@ guessBtn.addEventListener('click', function(){
   }
 
   // Check if won
- 
+  if(guess === winningNum){
     // Game over - won
-   
+    gameOver(true, `${winningNum} is correct, YOU WIN!`);
+
+  } else {
     // Wrong number
-    
+    guessesLeft -= 1;
+
+    if(guessesLeft === 0){
       // Game over - lost
-     
+      gameOver(false, `Game Over, you lost. The correct number was ${winningNum}`);
+    } else {
       // Game continues - answer wrong
 
       // Change border color
-    
+      guessInput.style.borderColor = 'red';
 
       // Clear Input
-     
+      guessInput.value = '';
 
       // Tell user its the wrong number
-  
+      setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, 'red');
+    }
+  }
 });
 
 // Game over
 function gameOver(won, msg){
- 
+  let color;
+  won === true ? color = 'green' : color = 'red';
 
   // Disable input
- 
+  guessInput.disabled = true;
   // Change border color
- 
+  guessInput.style.borderColor = color;
   // Set text color
- 
+  message.style.color = color;
   // Set message
-  
+  setMessage(msg);
 
   // PLay Again?
- 
+  guessBtn.value = 'Play Again';
+  guessBtn.className += 'play-again';
 }
 
 // Get Winning Number
 function getRandomNum(min, max){
-  
+  return Math.floor(Math.random()*(max-min+1)+min);
 }
 
 // Set message
 function setMessage(msg, color){
- 
+  message.style.color = color;
+  message.textContent = msg;
 }
