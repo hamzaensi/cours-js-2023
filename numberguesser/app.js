800973bfdfd43e12d85e9953cc254e198544ad6d
Hamza Zagrouba <hamzaensi@gmail.com>
2023-03-13 12:30:05 +0100

    Update app.js

--- numberguesser/app.js
+++ numberguesser/app.js
@@ -37,33 +37,18 @@ guessBtn.addEventListener('click', function(){
   let guess = parseInt(guessInput.value);
   
   // Validate
-  if(isNaN(guess) || guess < min || guess > max){
-    setMessage(`Please enter a number between ${min} and ${max}`, 'red');
-  }
+
 
   // Check if won
   if(guess === winningNum){
     // Game over - won
-    gameOver(true, `${winningNum} is correct, YOU WIN!`);
+ 
 
   } else {
     // Wrong number
-    guessesLeft -= 1;
-
-    if(guessesLeft === 0){
-      // Game over - lost
-      gameOver(false, `Game Over, you lost. The correct number was ${winningNum}`);
+   
     } else {
-      // Game continues - answer wrong
-
-      // Change border color
-      guessInput.style.borderColor = 'red';
-
-      // Clear Input
-      guessInput.value = '';
-
-      // Tell user its the wrong number
-      setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, 'red');
+      
     }
   }
 });
@@ -74,17 +59,16 @@ function gameOver(won, msg){
   won === true ? color = 'green' : color = 'red';
 
   // Disable input
-  guessInput.disabled = true;
+  
   // Change border color
-  guessInput.style.borderColor = color;
+ 
   // Set text color
-  message.style.color = color;
+  
   // Set message
-  setMessage(msg);
+  
 
   // PLay Again?
-  guessBtn.value = 'Play Again';
-  guessBtn.className += 'play-again';
+ 
 }
 
 // Get Winning Number
