e7431d090b6080a2038f102c40d8539975bcd802
Hamza Zagrouba <hamzaensi@gmail.com>
2023-03-13 15:39:05 +0100

    Update app.js

--- numberguesser/app.js
+++ numberguesser/app.js
@@ -14,7 +14,7 @@ let min = 1,
     guessesLeft = 3;
 
 // UI Elements
-      const game = document.querySelector('#game'),
+const game = document.querySelector('#game'),
       minNum = document.querySelector('.min-num'),
       maxNum = document.querySelector('.max-num'),
       guessBtn = document.querySelector('#guess-btn'),
@@ -37,18 +37,33 @@ guessBtn.addEventListener('click', function(){
   let guess = parseInt(guessInput.value);
   
   // Validate
-
+  if(isNaN(guess) || guess < min || guess > max){
+    setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+  }
 
   // Check if won
   if(guess === winningNum){
     // Game over - won
- 
+    gameOver(true, `${winningNum} is correct, YOU WIN!`);
 
   } else {
     // Wrong number
-   
+    guessesLeft -= 1;
+
+    if(guessesLeft === 0){
+      // Game over - lost
+      gameOver(false, `Game Over, you lost. The correct number was ${winningNum}`);
     } else {
-      
+      // Game continues - answer wrong
+
+      // Change border color
+      guessInput.style.borderColor = 'red';
+
+      // Clear Input
+      guessInput.value = '';
+
+      // Tell user its the wrong number
+      setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, 'red');
     }
   }
 });
@@ -59,16 +74,17 @@ function gameOver(won, msg){
   won === true ? color = 'green' : color = 'red';
 
   // Disable input
-  
+  guessInput.disabled = true;
   // Change border color
- 
+  guessInput.style.borderColor = color;
   // Set text color
-  
+  message.style.color = color;
   // Set message
-  
+  setMessage(msg);
 
   // PLay Again?
- 
+  guessBtn.value = 'Play Again';
+  guessBtn.className += 'play-again';
 }
 
 // Get Winning Number
